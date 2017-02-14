f8aa7d1aad5179940d20e3f4fc3d1f7a68c5b942
Patrick <pcrussel@ucsc.edu>
2017-02-14 13:12:36 -0800

    Adds Prototype Rooms
    Also adds temporary camera movement script

---
+++ Petty Dictator/Assets/Scripts/Camera/CameraSequence.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSequence : MonoBehaviour {
+    public float speed = 10;
+    public GameObject[] waypoints;
+
+	// Use this for initialization
+	void Start () {
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+        if(waypoints != null){
+            Vector3 initialPos = waypoints[0].transform.position;
+            transform.position = initialPos;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+    
+    void GoToWaypoint (int index){
+        if(waypoints.Length <= index)
+            Debug.Log("Sorry, too big");
+        float step = speed * Time.deltaTime;
+        Transform target = waypoints[index].transform;
+        
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        
+    }
+}
