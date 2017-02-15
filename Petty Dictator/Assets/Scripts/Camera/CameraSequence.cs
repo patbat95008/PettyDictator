fca5ace0128ec7dc439336c1a947003bb2b0e369
Patrick <pcrussel@ucsc.edu>
2017-02-15 08:04:13 -0800

    Adds Camera Movement
    Camera moves between scenes and returns to the start

--- Petty Dictator/Assets/Scripts/Camera/CameraSequence.cs
+++ Petty Dictator/Assets/Scripts/Camera/CameraSequence.cs
@@ -3,29 +3,37 @@ using System.Collections;
 
 public class CameraSequence : MonoBehaviour {
     public float speed = 10;
+	public int curr_index = 0;
+
     public GameObject[] waypoints;
 
+	private float dist_thresh = 0.1f;
 	// Use this for initialization
 	void Start () {
         waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
         if(waypoints != null){
-            Vector3 initialPos = waypoints[0].transform.position;
+			int last = waypoints.Length - 1;
+            Vector3 initialPos = waypoints[last].transform.position;
             transform.position = initialPos;
+			curr_index = last;
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+		float distance = Vector3.Distance(waypoints[curr_index].transform.position, transform.position);
+		if(distance > dist_thresh)
+			GoToWaypoint(curr_index);
 	}
     
     void GoToWaypoint (int index){
-        if(waypoints.Length <= index)
+		if(waypoints.Length <= index){
             Debug.Log("Sorry, too big");
-        float step = speed * Time.deltaTime;
-        Transform target = waypoints[index].transform;
-        
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
-        
+		}else{
+	        float step = speed * Time.deltaTime;
+	        Transform target = waypoints[index].transform;
+	        
+	        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+		}
     }
 }
---
+++ Petty Dictator/Assets/Scripts/UI/NextScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Next () {
+		GameObject go_cam = GameObject.Find("Main Camera");
+		CameraSequence cam_seq = go_cam.GetComponent<CameraSequence>();
+
+		if(cam_seq.curr_index > 0)
+			cam_seq.curr_index --;
+		else
+			cam_seq.curr_index = cam_seq.waypoints.Length - 1;
+	}
+}
